681cdcc576dea878d9de2a61494d4e5edb45ea71
dougm1963 <dougmeador1963@gmail.com>
2017-12-07 20:30:06 -0500

    final working changer

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,23 +1,11 @@
-def get_change(cents_received)
-	coins_returned = {}
-	coins = {"quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1}
-	
-	puts "Amount given is #{cents_received}"
-	amount_given = cents_received
+puts "How much does it cost? "
+a = gets.chomp.to_f
 
-coins.each do |coin, value|
-	if cents_received >= 0
-	coins_returned[coin] = cents_received / value
-	cents_received = cents_received - coin[value]
-	end
-	coins_returned
-		
-end
+puts "How much money given? "
+b = gets.chomp.to_f
 
-puts "#{amount_given} amounts to #{coins_returned[quarter]} quarters and #{coins_returned[dime]} dimes 
-and #{coins_returned[nickel]} nickels and #{coins_returned[penny]} pennies"
+c = #(b - a)
+puts "you have #{b - a} change."
 
-end
+ 
 
-
-get_change(66)
